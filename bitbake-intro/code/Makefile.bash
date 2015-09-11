73859447656acc939bf158e45abddbc5177d04d1
Vijay Kumar B <vijaykumar@zilogic.com>
2015-09-11 19:17:35 +0530

    Do not include image creation in Makefile.

--- bitbake-intro/code/Makefile.bash
+++ bitbake-intro/code/Makefile.bash
@@ -1,14 +1,12 @@
-TOOLCHAIN = /usr/share/gcc-arm-linux
 ROOTFS = ~/yp/make/rootfs
-DISKIMG = ~/yp/make/disk.img
 BDIR = ~/yp/make/bash-4.3
 
-all: disk-image
+all: install
 
-download:
+fetch:
 	cd ~/yp/dl; wget -c http://ftp.gnu.org/gnu/bash/bash-4.3.tar.gz
 
-unpack: download
+unpack: fetch
 	mkdir -p ~/yp/make
 	cd ~/yp/make; tar -x -f ~/yp/dl/bash-4.3.tar.gz
 
@@ -20,23 +18,8 @@ configure: unpack
 compile: configure
 	cd $(BDIR); make
 
-install-deps: compile
-	mkdir -p $(ROOTFS)/lib
-	cp $(TOOLCHAIN)/arm-none-linux-gnueabi/libc/lib/libc.so.6 \
-	   $(ROOTFS)/lib
-	cp $(TOOLCHAIN)/arm-none-linux-gnueabi/libc/lib/libdl.so.2 \
-	   $(ROOTFS)/lib
-	cp $(TOOLCHAIN)/arm-none-linux-gnueabi/libc/lib/ld-linux.so.3 \
-	   $(ROOTFS)/lib
-
-install: install-deps
+install: compile
 	cd $(BDIR); make install DESTDIR=$(ROOTFS)
 	mkdir -p $(ROOTFS)/bin
 	ln -f -s /usr/bin/bash $(ROOTFS)/bin/bash
 	ln -f -s /usr/bin/bash $(ROOTFS)/bin/sh
-
-
-disk-image: install
-	mkdir -p $(ROOTFS)/dev $(ROOTFS)/tmp
-	genext2fs -b 131072 -d $(ROOTFS) $(DISKIMG)
-
