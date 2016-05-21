afd69f207d585b9a5834ecf4f2271d8d13e1b460
Deepak Soundararajan <deepak@zilogic.com>
2016-05-22 00:06:16 +0530

    Made the sbrk implementation to use names from man, added heap.c as data-uri

--- arm-libc/code/heap.c
+++ arm-libc/code/heap.c
@@ -3,22 +3,17 @@
 extern char heap_low; /* Defined by the linker */
 extern char heap_top; /* Defined by the linker */
 
-char *heap_end = &heap_low;
+char *prog_break = &heap_low;
 
 caddr_t _sbrk(int incr) {
-	char *prev_heap_end;
-/*
-	if (heap_end == 0) {
-		heap_end = &heap_low;
-	}
-*/
-	prev_heap_end = heap_end;
+	char *prev_prog_break;
+	
+	prev_prog_break = prog_break;
 
-	if ((heap_end + incr) > &heap_top) {
-		/* Heap and stack collision */
+	if ((prog_break + incr) > &heap_top) {
 		return (caddr_t)0;
 	}
 
-	heap_end += incr;
-	return (caddr_t) prev_heap_end;
+	prog_break += incr;
+	return (caddr_t) prev_prog_break;
 }
