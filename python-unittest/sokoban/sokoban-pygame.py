85530b17d05579cf9cbb822962ce442473b7bef2
Vijay Kumar <vijaykumar@bravegnu.org>
2016-07-21 00:01:42 +0530

    Move most of the logic to the view layer.

--- python-unittest/sokoban/sokoban-pygame.py
+++ python-unittest/sokoban/sokoban-pygame.py
@@ -185,33 +185,63 @@ class World:
 class GameView:
     def __init__(self, gl):
         self.gl = gl
+        self._tile_size = gl.tile_size
         self._screen = None
+        self._width = None
+        self._height = None
         self._images = {}
         for tile in range(Tile.MAX):
             self._images[tile] = self.gl.load_tile(tile)
 
-    def set_size(self, width, height):
-        self._screen = self.gl.resize(width, height)
+    def _get_line_height(self):
+        _, dim = self.gl.render_string("|")
+        width, height = dim
+        return height
+
+    def set_size(self, ncols, nrows):
+        line_height = self._get_line_height()
+        self._width = ncols * self._tile_size
+        self._height = nrows * self._tile_size + line_height * 2
+
+        self._screen = self.gl.resize(self._width, self._height)
+
+    def _draw_status_line(self, world, moves):
+        x = 0
+        y = (world.nrows * self._tile_size)
+        img, _ = self.gl.render_string("MOVES  PUSHES")
+        self.gl.draw_image(self._screen, x, y, img)
+
+        y += self._get_line_height()
+        img, _ = self.gl.render_string(" {0:04}     {1:04}".format(len(moves), world.pushes))
+        self.gl.draw_image(self._screen, x, y, img)
 
     def show(self, world, moves):
         self.gl.clear(self._screen)
 
         for y in range(world.nrows):
             for x in range(world.ncols):
-                pos = Position(x, y)
-                tile = world.get(pos)
+                tile = world.get(Position(x, y))
                 if tile == None:
                     tile = Tile.WALL
-                self.gl.draw_tile(self._screen, x, y, self._images[tile])
 
-        self.gl.draw_status(self._screen,
-                            "MOVES  PUSHES",
-                            " {0:04}     {1:04}".format(len(moves), world.pushes))
+                sx = x * self._tile_size
+                sy = y * self._tile_size
+                img = self._images[tile]
+                self.gl.draw_image(self._screen, sx, sy, img)
+
+        self._draw_status_line(world, moves)
         self.gl.update(self._screen)
                 
     def show_msgbox(self, text):
         self.gl.clear(self._screen)
-        self.gl.draw_msgbox(self._screen, text)
+
+        img, dim = self.gl.render_string(text)
+        string_width, string_height = dim
+
+        x = self._width / 2 - string_width / 2
+        y = self._height / 2 - string_height / 2
+        self.gl.draw_image(self._screen, x, y, img)
+
         self.gl.update(self._screen)
         time.sleep(2)
 
@@ -223,11 +253,12 @@ class GameView:
 
 
 class PyGameGL:
-    def __init__(self, tile_size):
+    def __init__(self):
         pygame.init()
         pygame.display.set_caption("Sokoban!")
 
-        self._tile_size = tile_size
+        self.tile_size = 32
+
         self._tile_map = {
             Tile.WORKER: "worker",
             Tile.BOX: "box",
@@ -248,37 +279,20 @@ class PyGameGL:
             pygame.K_r: Key.RESET,
         }
         self._font = pygame.font.Font("atari.ttf", 12)
-        self._width = None
-        self._height = None
-        
+
     def resize(self, width, height):
-        self._width = width
-        self._height = height
-
-        text_surface = self._font.render("A", 0, (255, 255, 255), (0, 0, 0))
-        status_height = text_surface.get_rect().height * 2
-
-        dim = (width * self._tile_size, height * self._tile_size + status_height)
-        return pygame.display.set_mode(dim)
+        return pygame.display.set_mode((width, height))
 
     def load_tile(self, tile):
         tile_name = self._tile_map[tile]
         return pygame.image.load("yoshi-32-{0}.bmp".format(tile_name))
 
-    def draw_tile(self, screen, x, y, tile):
-        sx = x * self._tile_size
-        sy = y * self._tile_size
-        screen.blit(tile, (sx, sy))
+    def draw_image(self, screen, x, y, image):
+        screen.blit(image, (x, y))
 
-    def draw_status(self, screen, line1, line2):
-        sx = 0
-        sy = (self._height * self._tile_size)
-        text_surface = self._font.render(line1, 1, (255, 255, 255), (0, 0, 0))
-        screen.blit(text_surface, (sx, sy))
-
-        sy += text_surface.get_rect().height
-        text_surface = self._font.render(line2, 2, (255, 255, 255), (0, 0, 0))
-        screen.blit(text_surface, (sx, sy))        
+    def render_string(self, string):
+        surface = self._font.render(string, 1, (200, 200, 200), (0, 0, 0))
+        return surface, (surface.get_width(), surface.get_height())
 
     def clear(self, screen):
         screen.fill((0, 0, 0))
@@ -295,13 +309,6 @@ class PyGameGL:
                 return self._key_map[event.key]
             except KeyError:
                 return None
-
-    def draw_msgbox(self, screen, text):
-        text_surface = self._font.render(text, 1, (255, 255, 255), (0, 0, 0))
-        textpos = text_surface.get_rect()
-        textpos.centerx = screen.get_rect().centerx
-        textpos.centery = screen.get_rect().centery
-        screen.blit(text_surface, textpos)
 
 
 def play_level(view, level):
@@ -356,6 +363,6 @@ def main(view):
 
 
 if __name__ == "__main__":
-    gl = PyGameGL(32)
+    gl = PyGameGL()
     view = GameView(gl)
     main(view)
