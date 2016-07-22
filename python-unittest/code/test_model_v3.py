92cc3ecf7adbf18e74b67de02dc09bfb958d9aba
Vijay Kumar B <vijaykumar@zilogic.com>
2016-07-22 13:51:19 +0530

    Split the test code into fragments, and use them.g

--- python-unittest/code/test_model_v3.py
+++ python-unittest/code/test_model_v3.py
@@ -7,13 +7,6 @@ SIMPLE_LEVEL = [
     "#########",
 ]
 
-LARGE_LEVEL = [
-    "######## ",
-    "#.$ @  ##",
-    "#.$     #",
-    "#########",
-]
-
 class WorldTestCase(TestCase):
     def test_rect_world_dimensions(self):
         world = World(SIMPLE_LEVEL)
@@ -21,12 +14,7 @@ class WorldTestCase(TestCase):
         self.assertEqual(world.nrows, 3)
         self.assertEqual(world.ncols, 9)
 
-    def test_nonrect_world_dimenions(self):
-        world = World(LARGE_LEVEL)
-
-        self.assertEqual(world.nrows, 4)
-        self.assertEqual(world.ncols, 9)
-
+### START: tests.py
     def test_worker_pos(self):
         world = World(SIMPLE_LEVEL)
         self.assertEqual(world.worker_pos, (4, 1))
@@ -35,8 +23,4 @@ class WorldTestCase(TestCase):
         world = World(SIMPLE_LEVEL)
         self.assertEqual(world.box_pos, {(2, 1)})
         self.assertEqual(world.dock_pos, {(1, 1)})
-
-    def test_multiple_box_dock(self):
-        world = World(LARGE_LEVEL)
-        self.assertEqual(world.box_pos, {(2, 1), (2, 2)})
-        self.assertEqual(world.dock_pos, {(1, 1), (1, 2)})
+### END: tests.py
--- python-unittest/code/test_model_v4.py
+++ python-unittest/code/test_model_v4.py
@@ -7,6 +7,15 @@ SIMPLE_LEVEL = [
     "#########",
 ]
 
+### START: level.py
+LARGE_LEVEL = [
+    "######## ",
+    "#.$ @  ##",
+    "#.$     #",
+    "#########",
+]
+### END: level.py
+
 class WorldTestCase(TestCase):
     def test_rect_world_dimensions(self):
         world = World(SIMPLE_LEVEL)
@@ -22,3 +31,15 @@ class WorldTestCase(TestCase):
         world = World(SIMPLE_LEVEL)
         self.assertEqual(world.box_pos, {(2, 1)})
         self.assertEqual(world.dock_pos, {(1, 1)})
+
+### START: tests.py
+    def test_nonrect_world_dimenions(self):
+        world = World(LARGE_LEVEL)
+        self.assertEqual(world.nrows, 4)
+        self.assertEqual(world.ncols, 9)
+
+    def test_multiple_box_dock(self):
+        world = World(LARGE_LEVEL)
+        self.assertEqual(world.box_pos, {(2, 1), (2, 2)})
+        self.assertEqual(world.dock_pos, {(1, 1), (1, 2)})
+### END: tests.py
