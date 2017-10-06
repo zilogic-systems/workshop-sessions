00ab6f6f73b35b3651278c00ab7e48c1758a9b7a
Vijay Kumar B <vijaykumar@zilogic.com>
2017-10-06 20:46:01 +0530

    Improvement to PWM session.
      * Modified motor code to make the speed change visible
      * Minor tweaks to diagrams and slides
      * Removed PWM kernel internal details

--- uspace-pwm/code/dcmotor.py
+++ uspace-pwm/code/dcmotor.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 from pwm import *
-from gpio import *
 import time
 
 MOTOR = 1
 
 pwm_export_channel(MOTOR)
 
-period = 10000000
-pwm_set_period(MOTOR, period)
-duty = 7000000
-pwm_set_dutycycle(MOTOR, duty)
+period_ns = 10000000
+pwm_set_period(MOTOR, period_ns)
 
-pwm_enable(MOTOR)
-time.sleep(5)
-pwm_disable(MOTOR)
+while True:
+    for duty in [30, 100]:
+        duty_ns = int(period_ns * duty / 100)
+        pwm_set_dutycycle(MOTOR, duty_ns)
+
+        pwm_enable(MOTOR)
+        time.sleep(5)
+        pwm_disable(MOTOR)
 
 pwm_unexport_channel(MOTOR)
