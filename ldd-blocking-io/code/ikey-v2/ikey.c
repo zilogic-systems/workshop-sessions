87a32fc462b0f60de89c096db96f5c7262d04641
Vijay Kumar B <vijaykumar@zilogic.com>
2017-12-13 18:22:37 +0530

    Added code and slides for ldd-blocking-io session.

---
+++ ldd-blocking-io/code/ikey-v2/ikey.c
@@ -0,0 +1,136 @@
+/* ### START: all */
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/fs.h>
+#include <linux/i2c.h>
+#include <linux/wait.h>
+#include <linux/kfifo.h>
+#include <asm/uaccess.h>
+
+#define REG_STAT_ADDR  0x0
+#define REG_DATA_ADDR  0x1
+
+static struct i2c_client * client;
+static int major;
+
+static void ikey_poll(struct work_struct *work);
+DEFINE_KFIFO(ikey_fifo, char, 256);
+DECLARE_DELAYED_WORK(ikey_work, ikey_poll);
+static int ikey_poll_stop = 0;
+
+static void ikey_reg_read(char addr, char *data)
+{
+	i2c_master_send(client, &addr, 1);
+	i2c_master_recv(client, data, 1);
+}
+
+static bool ikey_get(char * ikey)
+{
+	char key_avail;
+
+	ikey_reg_read(REG_STAT_ADDR, &key_avail);
+	if (!key_avail)
+		return false;
+
+	ikey_reg_read(REG_DATA_ADDR, ikey);
+	return true;
+}
+
+/* ### START: poll */
+static void ikey_poll(struct work_struct *work)
+{
+	while (1) {
+		bool ret;
+		char key;
+
+		ret = ikey_get(&key);
+		if (ret == false)
+			break;
+		
+		kfifo_put(&ikey_fifo, key);
+	}
+
+	if (!ikey_poll_stop)
+		schedule_delayed_work(&ikey_work, HZ / 5);
+}
+/* ### END: poll */
+
+/* ### START: read */
+static ssize_t ikey_read(struct file *file, char __user * buf,
+			size_t count, loff_t * ppos)
+{
+	int i;
+
+	if (kfifo_is_empty(&ikey_fifo))
+	    return -EAGAIN;
+
+	for (i = 0; i < count; i++) {
+		char ikey;
+
+		if (kfifo_get(&ikey_fifo, &ikey) == 0)
+			break;
+		if (put_user(ikey + '0', buf + i))
+			return -EFAULT;
+	}
+
+	return i;
+}
+/* ### END: read */
+
+static const struct file_operations ikey_fops = {
+	.owner          = THIS_MODULE,
+	.read           = ikey_read
+};
+
+static int ikey_init(void)
+{
+	struct i2c_adapter * adapter;
+	struct i2c_board_info board_info = { .type = "ikey", .addr = 0x54 };
+	int err = 0;
+
+	adapter = i2c_get_adapter(0);
+	if (adapter == NULL) {
+		printk("ikey: error getting adapter");
+		err = -ENODEV;
+		goto err_exit;
+	}
+
+	client = i2c_new_device(adapter, &board_info);
+	if (client == NULL) {
+		printk("ikey: error creating client");
+		err = -ENODEV;
+		goto err_exit;
+	}
+
+	major = register_chrdev(0, "ikey", &ikey_fops);
+	if (major < 0) {
+		printk("ikey: could not register char. device\n");
+		err = major;
+		goto err_free_client;
+	}
+
+	schedule_delayed_work(&ikey_work, HZ / 5);
+
+	return 0;
+
+err_free_client:
+	i2c_unregister_device(client);
+
+err_exit:
+	return err;
+}
+
+static void ikey_exit(void)
+{
+	ikey_poll_stop = 1;
+	cancel_delayed_work_sync(&ikey_work);
+	
+	unregister_chrdev(major, "ikey");
+	i2c_unregister_device(client);
+}
+
+module_init(ikey_init);
+module_exit(ikey_exit);
+MODULE_LICENSE("GPL");
+/* ### END: all */
