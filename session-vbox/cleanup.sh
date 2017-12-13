78cda717685b7a623e7a56aecd345128c86faf4d
raashidtech <raashidmuhammed@zilogic.com>
2017-12-13 18:50:13 +0530

    Update cleanup script.

--- session-vbox/cleanup.sh
+++ session-vbox/cleanup.sh
@@ -2,5 +2,5 @@
 
 vboxmanage controlvm debian-i386-wheezy poweroff
 vboxmanage unregistervm debian-i386-wheezy  --delete
-rm -rf machines tmp vboximage
+rm -rf machines tmp vboximage *.vmdk *.ovf
 rm -f workshop-box.tar.gz
