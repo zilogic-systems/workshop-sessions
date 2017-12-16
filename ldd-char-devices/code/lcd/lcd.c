c863a85a01ee1397c3e27a03416289125e7eb340
Vijay Kumar B <vijaykumar@zilogic.com>
2017-12-16 06:55:47 +0530

    Add code for LCD driver.

--- ldd-char-devices/code/lcd/lcd.c
+++ ldd-char-devices/code/lcd/lcd.c
@@ -4,13 +4,37 @@
 #include <linux/types.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/i2c.h>
 
 #include <asm/uaccess.h>
 
+static int major;
+static struct i2c_client * client;
+
 static ssize_t lcd_write(struct file * file, const char __user * buf,
-			  size_t len, loff_t *ppos)
+			 size_t len, loff_t *ppos)
 {
-	/* Your I2C LCD Write Code Here */
+	size_t i;
+
+	for (i = 0; i < len; i++) {
+		char ch;
+		char msg[2];
+		int ret;
+
+		if (get_user(ch, buf + i))
+			return -EFAULT;
+		
+		msg[0] = 0x0;      /* Register Address: 0x0 */
+		msg[1] = ch;       /* Register Data: ch */
+
+		ret = i2c_master_send(client, msg, sizeof(msg));
+		if (ret != sizeof(msg)) {
+			printk("lcd: error sending data");
+			return -EIO;
+		}
+	}
+
+	return len;
 }
 
 static const struct file_operations lcd_fops = {
@@ -20,20 +44,45 @@ static const struct file_operations lcd_fops = {
 
 static int lcd_init(void)
 {
-        /* Your I2C LCD Init Code Here */
+	int ret;
+	struct i2c_adapter * adapter;
+	struct i2c_board_info board_info = { .type = "lcd", .addr = 0x40 };
 
-	major = register_chrdev(0, "lcd", &lcd_fops);
-	if (major < 0)
-		return major;
+	adapter = i2c_get_adapter(0);
+	if (adapter == NULL) {
+		printk("lcd: error getting adapter");
+		ret = -ENODEV;
+		goto out;
+	}
+
+	client = i2c_new_device(adapter, &board_info);
+	if (client == NULL) {
+		printk("lcd: error creating client");
+		ret = -EBUSY;
+		goto out;
+	}
+
+	ret = register_chrdev(0, "lcd", &lcd_fops);
+	if (ret < 0) {
+		printk("lcd: error registering char. device");
+		goto out_free_device;
+	}
+
+	major = ret;
 
 	return 0;
+
+out_free_device:
+	i2c_unregister_device(client);
+
+out:
+	return ret;
 }
 
 static void lcd_exit(void)
 {
-        /* Your I2C LCD Exit Code Here */
-
 	unregister_chrdev(major, "lcd");
+	i2c_unregister_device(client);
 }
 
 module_init(lcd_init);
