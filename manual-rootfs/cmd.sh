5f41f9488649f83d7e9d4baf534cd9372b6e9b7c
Raashid Muhammed <raashidmuhammed@zilogic.com>
2023-11-21 13:14:26 +0530

    Update Manual Rootfs session slides.

--- manual-rootfs/cmd.sh
+++ manual-rootfs/cmd.sh
@@ -2,7 +2,7 @@
 
 set -e -x -u
 
-SHARED=~/yp/shared
+SHARED=~/mr/shared
 
 cd $SHARED
 cat <<EOF > run-qemu.sh
@@ -16,9 +16,9 @@ qemu-system-arm -M versatilepb \
 EOF
 
 ### START: copy-prebuilt.sh
-cp ~/yp/pre-built/zImage $SHARED
-cp ~/yp/pre-built/disk.img $SHARED
-cp ~/yp/pre-built/versatile-pb.dtb $SHARED
+cp ~/mr/pre-built/zImage $SHARED
+cp ~/mr/pre-built/disk.img $SHARED
+cp ~/mr/pre-built/versatile-pb.dtb $SHARED
 ### END: copy-prebuilt.sh
 
 ### START: run-qemu.sh
@@ -27,11 +27,11 @@ bash run-qemu.sh
 ### END: run-qemu.sh
 
 ### START: setup-env.sh
-ROOTFS=~/yp/manual/rootfs
-DISKIMG=~/yp/manual/disk.img
+ROOTFS=~/mr/manual/rootfs
+DISKIMG=~/mr/manual/disk.img
 ### END: setup-env.sh
 
-cat > ~/yp/manual/hello.c <<EOF
+cat > ~/mr/manual/hello.c <<EOF
 /* ### START: hello.c */
 #include <stdio.h>
 #include <unistd.h>
@@ -47,20 +47,20 @@ int main()
 EOF
 
 ### START: build-hello.sh
-cd ~/yp/manual
+cd ~/mr/manual
 arm-linux-gnueabi-gcc -static hello.c -o hello
 ### END: build-hello.sh
 
 ### START: copy-hello.sh
-mkdir -p $ROOTFS/bin
-cp hello $ROOTFS/bin/sh
+mkdir -p ~/mr/manual/rootfs/bin
+cp hello ~/mr/manual/rootfs/bin/sh
 ### END: copy-hello.sh
 
 ### START: create-hello-rootfs.sh
-genext2fs -b 131072 -d $ROOTFS $DISKIMG
+genext2fs -b 131072 -d ~/mr/manual/rootfs ~/mr/manual/disk.img
 ### END: create-hello-rootfs.sh
 
-cp $DISKIMG $SHARED
+cp ~/mr/manual/disk.img $SHARED
 
 ### START: boot-hello-rootfs.sh
 cd $SHARED
@@ -68,13 +68,13 @@ bash run-qemu.sh
 ### END: boot-hello-rootfs.sh
 
 ### START: download-bash.sh
-cd ~/yp/dl
+cd ~/mr/dl
 wget -c http://ftp.gnu.org/gnu/bash/bash-4.3.tar.gz
 ### END: download-bash.sh
 
 ### START: unpack-bash.sh
-cd ~/yp/manual
-tar -x -f ~/yp/dl/bash-4.3.tar.gz
+cd ~/mr/manual
+tar -x -f ~/mr/dl/bash-4.3.tar.gz
 ### END: unpack-bash.sh
 
 ### START: configure-bash.sh
@@ -89,43 +89,43 @@ make
 ### END: build-bash.sh
 
 ### START: install-bash.sh
-make install DESTDIR=$ROOTFS
+make install DESTDIR=~/mr/manual/rootfs
 ### END: install-bash.sh
 
 ### START: check-bash-deps.sh
-arm-linux-gnueabi-readelf -d $ROOTFS/usr/bin/bash
+arm-linux-gnueabi-readelf -d ~/mr/manual/rootfs/usr/bin/bash
 ### END: check-bash-deps.sh
 
-rm -f $ROOTFS/bin/sh
+rm -f ~/mr/manual/rootfs/bin/sh
 
 ### START: link-bash.sh
-ln -s /usr/bin/bash $ROOTFS/bin/bash
-ln -s /usr/bin/bash $ROOTFS/bin/sh
+ln -s /usr/bin/bash ~/mr/manual/rootfs/bin/bash
+ln -s /usr/bin/bash ~/mr/manual/rootfs/bin/sh
 ### END: link-bash.sh
 
 ### START: cp-bash-deps.sh
-mkdir $ROOTFS/lib
+mkdir ~/mr/manual/rootfs/lib
 TOOLCHAIN="/usr/arm-linux-gnueabi"
 cp $TOOLCHAIN/lib/libc.so.6 \
-   $ROOTFS/lib
+   ~/mr/manual/rootfs/lib
 cp $TOOLCHAIN/lib/libdl.so.2 \
-   $ROOTFS/lib
+   ~/mr/manual/rootfs/lib
 ### END: cp-bash-deps.sh
 
 ### START: copy-ld.sh
 cp $TOOLCHAIN/lib/ld-linux.so.3 \
-   $ROOTFS/lib
+   ~/mr/manual/rootfs/lib
 ### END: copy-ld.sh
 
 ### START: make-dev-tmp.sh
-mkdir $ROOTFS/dev $ROOTFS/tmp
+mkdir ~/mr/manual/rootfs/dev ~/mr/manual/rootfs/tmp
 ### END: make-dev-tmp.sh
 
 ### START: create-bash-rootfs.sh
-genext2fs -b 131072 -d $ROOTFS $DISKIMG
+genext2fs -b 131072 -d ~/mr/manual/rootfs ~/mr/manual/disk.img
 ### END: create-bash-rootfs.sh
 
-cp $DISKIMG $SHARED
+cp ~/mr/manual/disk.img $SHARED
 
 ### START: boot-bash-rootfs.sh
 cd $SHARED
@@ -133,62 +133,62 @@ bash run-qemu.sh
 ### END: boot-bash-rootfs.sh
 
 ### START: build-coreutils.sh
-cd ~/yp/dl
+cd ~/mr/dl
 wget -c http://ftp.gnu.org/gnu/coreutils/coreutils-8.32.tar.xz
-cd ~/yp/manual
-tar -x -f ~/yp/dl/coreutils-8.32.tar.xz
+cd ~/mr/manual
+tar -x -f ~/mr/dl/coreutils-8.32.tar.xz
 cd coreutils-8.32
 ./configure --prefix=/usr                 \
             --host=arm-linux-gnueabi \
             --build=i686-pc-linux-gnu
 make
-make install DESTDIR=$ROOTFS
+make install DESTDIR=~/mr/manual/rootfs
 ### END: build-coreutils.sh
 
 ### START: create-ls-rootfs.sh
-genext2fs -b 131072 -d $ROOTFS $DISKIMG
+genext2fs -b 131072 -d ~/mr/manual/rootfs ~/mr/manual/disk.img
 ### END: create-ls-rootfs.sh
 
-cp $DISKIMG $SHARED
+cp ~/mr/manual/disk.img $SHARED
 
 cd $SHARED
 bash run-qemu.sh
 
 ### START: check-ls-deps.sh
-arm-linux-gnueabi-readelf -d $ROOTFS/usr/bin/ls
+arm-linux-gnueabi-readelf -d ~/mr/manual/rootfs/usr/bin/ls
 ### END: check-ls-deps.sh
 
 cp $TOOLCHAIN/lib/librt.so.1 \
-   $ROOTFS/lib
+   ~/mr/manual/rootfs/lib
 
-genext2fs -b 131072 -d $ROOTFS $DISKIMG
+genext2fs -b 131072 -d ~/mr/manual/rootfs ~/mr/manual/disk.img
 
 qemu-system-arm                 \
   -M versatilepb                \
-  -kernel ~/yp/pre-built/zImage \
+  -kernel ~/mr/pre-built/zImage \
   -append "root=/dev/sda rw"    \
   -dtb versatile-pb.dtb         \
-  -hda $DISKIMG
+  -hda ~/mr/manual/disk.img
 
 cp $TOOLCHAIN/lib/libpthread.so.0 \
-   $ROOTFS/lib
+   ~/mr/manual/rootfs/lib
 
-genext2fs -b 131072 -d $ROOTFS $DISKIMG
+genext2fs -b 131072 -d ~/mr/manual/rootfs ~/mr/manual/disk.img
 
 qemu-system-arm                 \
   -M versatilepb                \
-  -kernel ~/yp/pre-built/zImage \
+  -kernel ~/mr/pre-built/zImage \
   -append "root=/dev/sda rw"    \
   -dtb versatile-pb.dtb         \
-  -hda $DISKIMG
+  -hda ~/mr/manual/disk.img
 
 
 set +e
 ### START: build-less-fail.sh
-cd ~/yp/dl
+cd ~/mr/dl
 wget -c http://www.greenwoodsoftware.com/less/less-458.tar.gz
-cd ~/yp/manual
-tar -x -f ~/yp/dl/less-458.tar.gz
+cd ~/mr/manual
+tar -x -f ~/mr/dl/less-458.tar.gz
 cd less-458
 ./configure --prefix=/usr                 \
             --host=arm-linux-gnueabi \
@@ -197,30 +197,31 @@ cd less-458
 set -e
 
 ### START: build-ncurses.sh
-cd ~/yp/dl
+cd ~/mr/dl
 wget -c http://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz
-cd ~/yp/manual
-tar -x -f ~/yp/dl/ncurses-6.2.tar.gz
+cd ~/mr/manual
+tar -x -f ~/mr/dl/ncurses-6.2.tar.gz
 cd ncurses-6.2
 ./configure --prefix=/usr             \
             --host=arm-linux-gnueabi  \
             --build=i686-pc-linux-gnu \
 	    --without-progs           \
-	    --disable-ext-funcs
+	    --disable-ext-funcs       \
+      --without-cxx
 make
-make install DESTDIR=$ROOTFS
+make install DESTDIR=~/mr/manual/rootfs
 ### END: build-ncurses.sh
 
 set +e
 ### START: build-less-fail-2.sh
-cd ~/yp/manual/less-458
+cd ~/mr/manual/less-458
 ./configure --prefix=/usr                 \
             --host=arm-linux-gnueabi \
             --build=i686-pc-linux-gnu
 ### END: build-less-fail-2.sh
 set -e
 
-cat > ~/yp/manual/hello-ncurses.c <<EOF
+cat > ~/mr/manual/hello-ncurses.c <<EOF
 /* ### START: hello-ncurses.c */
 #include <ncurses.h>
 
@@ -239,42 +240,42 @@ EOF
 set +e
 
 ### START: build-hello-ncurses.sh
-cd ~/yp/manual
+cd ~/mr/manual
 arm-linux-gnueabi-gcc hello-ncurses.c -o hello-ncurses
 ### END: build-hello-ncurses.sh
 
 set -e
 
 ### START: check-ncurses-location.sh
-ls $ROOTFS/usr/include
-ls $ROOTFS/usr/lib
+ls ~/mr/manual/rootfs/usr/include
+ls ~/mr/manual/rootfs/usr/lib
 ### END: check-ncurses-location.sh
 
 ### START: build-hello-ncurses-2.sh
 arm-linux-gnueabi-gcc hello-ncurses.c \
-    -I $ROOTFS/usr/include             \
-    -L $ROOTFS/usr/lib                 \
+    -I ~/mr/manual/rootfs/usr/include             \
+    -L ~/mr/manual/rootfs/usr/lib                 \
     -lncurses
 ### END: build-hello-ncurses-2.sh
 
 ### START: build-less.sh
-cd ~/yp/manual/less-458
+cd ~/mr/manual/less-458
 ./configure --prefix=/usr                 \
             --host=arm-linux-gnueabi \
             --build=i686-pc-linux-gnu     \
-            LDFLAGS="-L $ROOTFS/usr/lib"  \
-            CPPFLAGS="-I $ROOTFS/usr/include"
+            LDFLAGS="-L ~/mr/manual/rootfs/usr/lib"  \
+            CPPFLAGS="-I ~/mr/manual/rootfs/usr/include"
 make
-make install DESTDIR=$ROOTFS
+make install DESTDIR=~/mr/manual/rootfs
 ### END: build-less.sh
 
-genext2fs -b 131072 -d $ROOTFS $DISKIMG
+genext2fs -b 131072 -d ~/mr/manual/rootfs ~/mr/manual/disk.img
 
 cd $SHARED
 
 qemu-system-arm                 \
   -M versatilepb                \
-  -kernel ~/yp/pre-built/zImage \
+  -kernel ~/mr/pre-built/zImage \
   -append "root=/dev/sda rw"    \
   -dtb versatile-pb.dtb         \
-  -hda $DISKIMG
+  -hda ~/mr/manual/disk.img
